45644d4e73f172b9ae1e41a74dd371a6bc218733
黄远皓 <huangyuanhao@2379589812.vpn.umich.net>
2020-02-17 09:06:22 -0500

    Fixed bugs

--- paired_alignment.py
+++ paired_alignment.py
@@ -10,11 +10,11 @@ def main(map1, map2, outDir):
     out = os.path.join(outDir, 'paired_alignment')
     out_dedup = out+'_dedup'
 
-    # # pairing alignment
-    # cmd = f'join {map1} {map2} > {out}'
-    # print(cmd)
-    # print('pairing alignment')
-    # os.system(cmd)
+    # pairing alignment
+    cmd = f'join {map1} {map2} > {out}'
+    print(cmd)
+    print('pairing alignment')
+    os.system(cmd)
 
     # remove duplicates
     awk1 = '{s1 =\"+\"; s2 =\"+\"; if ($2 ==\"16\") {s1 =\"-\"}; if ($6 ==\"16\") {s2 =\"-\"}; print $3\"\\t\"$4\"\\t\"s1\"\\t\"$7\"\\t\"$8\"\\t\"s2\"\\t\"$5\"\\t\"$9\"\\t\"$1}'
